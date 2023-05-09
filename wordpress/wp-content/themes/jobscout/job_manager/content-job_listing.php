f38778338a24107493b45216e0a5f5a5945583d8
nguyenthihong190402 <105511378+nguyenthihong190402@users.noreply.github.com>
2023-05-10 00:54:34 +0700

    dsfdf

--- wordpress/wp-content/themes/jobscout/job_manager/content-job_listing.php
+++ wordpress/wp-content/themes/jobscout/job_manager/content-job_listing.php
@@ -21,7 +21,6 @@ global $post;
 $job_salary   = get_post_meta(get_the_ID(), '_job_salary', true);
 $job_featured = get_post_meta(get_the_ID(), '_featured', true);
 $company_name = get_post_meta(get_the_ID(), '_company_name', true);
-$job_date   = get_post_meta(get_the_ID(), '_job_date', true);
 ?>
 <article <?php job_listing_class(); ?> data-longitude="<?php echo esc_attr($post->geolocation_lat); ?>" data-latitude="<?php echo esc_attr($post->geolocation_long); ?>">
 
@@ -33,13 +32,9 @@ $job_date   = get_post_meta(get_the_ID(), '_job_date', true);
 		<h2 class="entry-title">
 			<a href="<?php the_job_permalink(); ?>"><?php wpjm_the_job_title(); ?></a>
 		</h2>
+		<p><time datetime="<?php echo get_the_date(); ?>"><?php echo "Create: ". get_the_date('M j, Y'); ?></time></p>
 
-		<?php if ($job_date) : ?>
-			<p class="job_date"><?php echo esc_html($job_date); ?></p>
-		<?php endif; ?>
-		<p><time datetime="<?php echo get_the_date('M  j, Y'); ?>" itemprop="datePublished"><?php echo get_the_date(); ?></time></p>
-
-		<div class="row">
+		<div class="row bg">
 			<div class="col-md-4">
 				<?php
 				if (get_option('job_manager_enable_types')) {
@@ -63,7 +58,6 @@ $job_date   = get_post_meta(get_the_ID(), '_job_date', true);
 
 			<div class="col-md-4">
 				<div class="company-address">
-					<i class="fas fa-map-marker-alt"></i>
 					<?php the_job_location(true); ?>
 				</div>
 			</div>
